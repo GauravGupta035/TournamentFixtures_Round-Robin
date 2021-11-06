d43d425ecce6fb40fa1334d03cb68b68f76e08ee
sayantan_codes <54500838+Sayantan-codes@users.noreply.github.com>
2021-11-06 23:47:03 +0530

    Update GUI.py
    Fixture Generation

--- GUI.py
+++ GUI.py
@@ -1,7 +1,32 @@
 from tkinter import *
 from tkinter.ttk import *
 
+'''
+#Fixture generation code
 
+def arr_rotate(arr):
+    x = []
+    x.append(arr[-2])
+    for i in range(0, len(arr) - 2):
+        x.append(arr[i])
+    x.append(arr[-1])
+    return x
+def schedule(team_list):
+    l = len(team_list)
+    rotate = team_list
+    team_map={1:'DC',2:'PBKS',3:'RR',4:'KKR',5:'MI',6:'RCB',7:'SRH',8:'CSK'}
+    for i in range(l-1):
+        rotate = arr_rotate(rotate)
+        left = 0
+        right = l-1
+        print("-----Round ",i+1," Fixtures-----")
+        while(left<right):
+            print("Team ",team_map[rotate[left]]," Plays Team ",team_map[rotate[right]])
+            left+=1
+            right-=1
+l = list(map(int,input("Enter list ").split()))
+schedule(l)
+'''
 class Fixture:
     def __init__(self, master=None):
         self.master = master
